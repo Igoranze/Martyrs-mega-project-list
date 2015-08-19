8138a6e774a01fa17512eee717761cd21bb046cb
Gligor Selimovic <selimovic@ws0089.serviceplanet.nl>
2015-08-19 17:41:36 +0200

    String reverse finished

--- reverse-a-string/src/main/application/Main.java
+++ reverse-a-string/src/main/application/Main.java
@@ -3,8 +3,12 @@ package main.application;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Creates object to store the string in
+		StringBuilder stringB = new StringBuilder(); 
+		//Append a string to the stringobject (StringBuilder class)
+		stringB.append("This string to reverse");
+		//Print the reversed string to the console via the reverse() method
+		System.out.println(stringB.reverse());
 	}
 
 }
