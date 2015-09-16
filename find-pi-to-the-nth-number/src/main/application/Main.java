25b7332b6bbd2f6cf28b1e5bc14a3e033d1767b6
Igoranze <g-selimovic@live.nl>
2015-09-16 15:03:35 +0200

    Changed gitignore file

---
+++ find-pi-to-the-nth-number/src/main/application/Main.java
@@ -0,0 +1,58 @@
+package main.application;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter num for decimals");
+		int num = Integer.parseInt(in.nextLine());
+		
+		System.out.println(bigPi(num, num));
+
+	}
+	
+	/*
+	 * This is a quick and dirty implementation of Bellard's formula bigPi(200,2000) is good for over 500 decimal places in 75ms.
+	 */
+	public static BigDecimal bigPi(int max,int digits) {
+	    BigDecimal num2power6 = new BigDecimal(64);
+	    BigDecimal sum = new BigDecimal(0);
+	    for(int i = 0; i < max; i++ ) {
+	        BigDecimal tmp;
+	        BigDecimal term ; 
+	        BigDecimal divisor;
+	        term = new BigDecimal(-32); 
+	        divisor = new BigDecimal(4*i+1); 
+	        tmp =  term.divide(divisor, digits, BigDecimal.ROUND_FLOOR);
+	        term = new BigDecimal(-1); 
+	        divisor = new BigDecimal(4*i+3); 
+	        tmp = tmp.add(term.divide(divisor, digits, BigDecimal.ROUND_FLOOR));
+	        term = new BigDecimal(256); 
+	        divisor = new BigDecimal(10*i+1); 
+	        tmp = tmp.add(term.divide(divisor, digits, BigDecimal.ROUND_FLOOR));
+	        term = new BigDecimal(-64); 
+	        divisor = new BigDecimal(10*i+3); 
+	        tmp = tmp.add(term.divide(divisor, digits, BigDecimal.ROUND_FLOOR));
+	        term = new BigDecimal(-4); 
+	        divisor = new BigDecimal(10*i+5); 
+	        tmp = tmp.add(term.divide(divisor, digits, BigDecimal.ROUND_FLOOR));
+	        term = new BigDecimal(-4); 
+	        divisor = new BigDecimal(10*i+7); 
+	        tmp = tmp.add(term.divide(divisor, digits, BigDecimal.ROUND_FLOOR));
+	        term = new BigDecimal(1); 
+	        divisor = new BigDecimal(10*i+9); 
+	        tmp = tmp.add(term.divide(divisor, digits, BigDecimal.ROUND_FLOOR));
+	        int s = ((1-((i&1)<<1)));
+	        divisor = new BigDecimal(2); 
+	        divisor = divisor.pow(10*i).multiply(new BigDecimal(s));
+	        sum = sum.add(tmp.divide(divisor, digits, BigDecimal.ROUND_FLOOR));
+	    }
+	    sum = sum.divide(num2power6,digits, BigDecimal.ROUND_FLOOR);
+	    return sum;
+
+	}
+
+}
