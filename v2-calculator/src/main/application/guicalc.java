6ad99db2d47432a353173260fa043ae3565c5fef
Igoranze <g-selimovic@live.nl>
2015-09-17 09:03:24 +0200

    Changed gitignore file

---
+++ v2-calculator/src/main/application/guicalc.java
@@ -0,0 +1,44 @@
+package main.application;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class guicalc {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					guicalc window = new guicalc();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public guicalc() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setResizable(false);
+		frame.setBounds(100, 100, 800, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
